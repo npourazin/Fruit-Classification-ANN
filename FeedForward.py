65118cf0c4aa5cac0faf64290d3a51e9c7efdd4a
Niki Pourazin <nikipourzin@gmail.com>
2021-11-18 17:47:39 +0330

    Implement ANN to check random weight accuracy on a part of train set

--- FeedForward.py
+++ FeedForward.py
@@ -6,7 +6,54 @@ train_set = ld.load_and_get_set(test_or_train="train",
                                 label_file_path="ANN_Project_Assets/Datasets/train_set_labels.pkl")
 
 layer_sizes = [len(train_set[0][0]), 150, 60, 4]  # [102, 150, 60, 4]
-print(layer_sizes)
-print(layer_sizes[1])
+
+# There are 4 layers, between each two consecutive layers, there needs to be a weight matrix
+W = [
+    np.random.normal(size=(layer_sizes[1], layer_sizes[0])),  # weights between layer 0 and 1, aka W[0]
+    np.random.normal(size=(layer_sizes[2], layer_sizes[1])),  # weights between layer 1 and 2, aka W[1]
+    np.random.normal(size=(layer_sizes[3], layer_sizes[2]))  # weights between layer 2 and 3, aka W[2]
+]
+
+# Initialize bias to 0, for every layer.
+B = [
+    np.zeros((layer_sizes[1], 1)),  # bias vector between layer 0 and 1, aka B[0]
+    np.zeros((layer_sizes[2], 1)),  # bias vector between layer 1 and 2, aka B[1]
+    np.zeros((layer_sizes[3], 1)),  # bias vector between layer 2 and 3, aka B[2]
+]
 
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def check_accuracy(calculated_labels, correct_labels):
+    calculated_ans = np.where(calculated_labels == np.amax(calculated_labels))
+    correct_ans = np.where(correct_labels == np.amax(correct_labels))
+
+    return calculated_ans == correct_ans
+
+
+def run_network_once():
+    data_size = 200
+    correct_ans_count = 0
+
+    for td in train_set[:data_size]:
+        # print("label", td[1])
+        z = [
+            np.zeros((layer_sizes[0], 1)),
+            np.zeros((layer_sizes[1], 1)),
+            np.zeros((layer_sizes[2], 1)),
+            np.zeros((layer_sizes[3], 1))
+
+        ]
+        z[0] = td[0]
+        np.reshape(z[0], (102, 1))
+        for i in range(1, 4):
+            z[i] = sigmoid(W[i - 1] @ z[i - 1] + B[i - 1])
+
+        if check_accuracy(z[3], td[1]):
+            correct_ans_count += 1
+
+    print(correct_ans_count/data_size)
+
+run_network_once()
